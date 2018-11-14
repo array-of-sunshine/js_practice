7c02ec35763138c425ad495a693b21aa4a98a30b
Brian <riceb53@gmail.com>
2018-11-13 19:32:37 -0600

    add styling with button

--- js_dom/alex.js
+++ js_dom/alex.js
@@ -1,3 +1,12 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 function hideParagraph() {
   console.log('hello');
   // select a DOM element from the HTML page
@@ -12,7 +21,14 @@ function hideParagraph() {
   pTag.classList.toggle('hidden');
 }
 
-
+function makeHeaderRed() {
+  console.log('making the header red...');
+  var header = document.querySelector('h1');
+  console.log(header);
+  // header.classList.add('red');
+  header.style.color = getRandomColor();
+  header.classList.add('add-padding');
+}
 
 // allow user to run js
 // select a DOM element from the HTML page
